2d7e8db257a038a7fb8e44d8ad0818a746ec1c9b
wangmingzhu <49201650@qq.com>
2025-10-23 19:05:43 +0800

    fix: fixed List

--- src/components/List/demos/Main/index.jsx
+++ src/components/List/demos/Main/index.jsx
@@ -17,38 +17,8 @@ export default () => {
         itemLayout="vertical"
         // pagination
         value={value}
-        reload={true}
         loadData={({ page }) => {
           // 兼容演示：将原有数组封装为新返回结构
-          return {
-            status: listData?.length ? undefined : 'empty',
-            message: '',
-            page: page,
-            rows: 20,
-            list: listData,
-            totalPage: undefined,
-            totalRows: listData?.length || 0
-          }
-        }}
-        onChange={(value) => {
-          console.log(value)
-          setValue(value)
-        }}
-      />
-      <Page.Header className="text-center">Layout horizontal</Page.Header>
-      <List.Main
-        ref={mainRef}
-        prepend={({ list, value, onChange, pagination }) => {
-          // console.log({ list, value, onChange, pagination })
-          return <div>Custom prepend</div>
-        }}
-        itemLayout="horizontal"
-        // true: 默认Card包裹Item
-        itemRender={true}
-        // pagination
-        value={value}
-        reload={true}
-        loadData={({ page }) => {
           return {
             status: listData?.length ? undefined : 'empty',
             message: '',
